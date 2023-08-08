4d5608878f6e3d853480bd6f24b1236251a9a523
vocu <101651423+vocu@users.noreply.github.com>
2023-08-08 12:14:26 +0200

    Fix dur_fmt

--- src/other.v
+++ src/other.v
@@ -15,7 +15,6 @@ pub fn (c Cmd) success(s string) {
 pub fn dur_fmt(sw time.StopWatch) string {
 	if sw.elapsed().milliseconds() < 1000 {
 		f := 1000000.0
-		println(sw.elapsed().nanoseconds())
 		return '(' + strconv.f64_to_str_l(math.round_sig(sw.elapsed().nanoseconds()/f, 1)) + 'ms)'
 	} else if sw.elapsed().milliseconds() < 60000 {
 		return '(' + (sw.elapsed().milliseconds() / 1000).str() + 's)'
