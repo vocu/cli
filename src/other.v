537189c659aaf399510aae64c7e4b270182ee8b9
vocu <101651423+vocu@users.noreply.github.com>
2023-08-08 12:09:43 +0200

    Small change in dur_fmt

--- src/other.v
+++ src/other.v
@@ -1,6 +1,8 @@
 module cli
 
 import time
+import math
+import strconv
 
 pub fn (c Cmd) fail(s string) {
 	eprintln(c.color.red + ' × ' + c.color.end + s)
@@ -12,7 +14,9 @@ pub fn (c Cmd) success(s string) {
 
 pub fn dur_fmt(sw time.StopWatch) string {
 	if sw.elapsed().milliseconds() < 1000 {
-		return '(' + sw.elapsed().milliseconds().str() + 'ms)'
+		f := 1000000.0
+		println(sw.elapsed().nanoseconds())
+		return '(' + strconv.f64_to_str_l(math.round_sig(sw.elapsed().nanoseconds()/f, 1)) + 'ms)'
 	} else if sw.elapsed().milliseconds() < 60000 {
 		return '(' + (sw.elapsed().milliseconds() / 1000).str() + 's)'
 	} else {
